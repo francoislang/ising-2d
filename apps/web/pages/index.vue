bf95c8f48f135121af13a92124a01fd7a347e7d9
francoislang <langfrancoisfrederic@gmail.com>
2026-01-31 17:55:08 +0100

    wip(app): config store + prettier

---
+++ apps/web/pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  const isingStore = useIsingStore();
+</script>
+
+<template>
+  <div>
+    <div>
+      <label for="">Taille du réseau (L): {{isingStore.network_length*isingStore.network_length}}</label>
+      <input type="range" min="64" max="128">
+    </div>
+  </div>
+</template>
