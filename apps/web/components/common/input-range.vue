ce4b3d62df3a195a70ce58a94221d2759a5faffe
francoislang <langfrancoisfrederic@gmail.com>
2026-02-07 15:11:43 +0100

    wip(app): inpute range

--- apps/web/components/common/input-range.vue
+++ apps/web/components/common/input-range.vue
@@ -8,16 +8,15 @@
       max?: number;
       step?: number;
       label?: string;
-      place?: string;
       size?: string;
     }>(),
     {
-      place: 'top',
-      size: 'xs',
+      size: 'sm',
     },
   );
 
   const isDragging = ref(false);
+  const isHovering = ref(false);
   const sliderContainer = ref<HTMLElement | null>(null);
 
   const min = computed(() => props.min ?? 0);
@@ -28,65 +27,47 @@
     return ((props.modelValue - min.value) / (max.value - min.value)) * 100;
   });
 
-  const placeTitleClass = computed(() => {
-    switch (props.place) {
-      case 'top':
-        return 'flex flex-col';
-      case 'bottom':
-        return 'flex flex-col-reverse';
-      case 'left':
-        return 'flex flex-row';
-      case 'right':
-        return 'flex flex-row-reverse';
-    }
-  });
-
   const sizeConfig = computed(() => {
     switch (props.size) {
       case 'xs':
         return {
           text: 'text-xs',
           track: 'h-1',
-          handle: 'w-3 h-3',
-          valueOffset: '-top-4',
+          handle: 'w-0.5 h-4',
+          valueOffset: '-top-2',
           container: 'h-6',
-          minWidth: 'min-w-16',
         };
       case 'sm':
         return {
           text: 'text-sm',
           track: 'h-1.5',
-          handle: 'w-4 h-4',
-          valueOffset: '-top-5',
+          handle: 'w-0.5 h-5',
+          valueOffset: '-top-3',
           container: 'h-7',
-          minWidth: 'min-w-24',
         };
       case 'md':
         return {
           text: 'text-base',
           track: 'h-2',
-          handle: 'w-5 h-5',
-          valueOffset: '-top-6',
+          handle: 'w-1 h-6',
+          valueOffset: '-top-4',
           container: 'h-8',
-          minWidth: 'min-w-32',
         };
       case 'lg':
         return {
           text: 'text-lg',
           track: 'h-3',
-          handle: 'w-6 h-6',
-          valueOffset: '-top-7',
+          handle: 'w-1 h-8',
+          valueOffset: '-top-5',
           container: 'h-10',
-          minWidth: 'min-w-40',
         };
       default:
         return {
           text: 'text-base',
           track: 'h-2',
-          handle: 'w-5 h-5',
+          handle: 'w-1 h-6',
           valueOffset: '-top-6',
           container: 'h-8',
-          minWidth: 'min-w-32',
         };
     }
   });
@@ -139,18 +120,18 @@
 </script>
 
 <template>
-  <div class="w-full flex gap-4 items-center justify-between" :class="[placeTitleClass]">
-    <label
-      v-if="label"
-      class="text-gray-700 block shrink-0 whitespace-nowrap"
-      :class="sizeConfig.text"
-    >
+  <div
+    class="w-full flex flex-col gap-1"
+    @mouseover="isHovering = true"
+    @mouseout="isHovering = false"
+  >
+    <label v-if="label" class="text-gray-500 text-xs pb-1">
       {{ label }}
     </label>
     <div
       ref="sliderContainer"
-      class="relative flex-1 flex items-center cursor-pointer select-none touch-none"
-      :class="[sizeConfig.container, sizeConfig.minWidth]"
+      class="relative w-full flex items-center cursor-pointer select-none touch-none"
+      :class="sizeConfig.container"
       @mousedown.prevent="handleMouseDown"
     >
       <div class="w-full bg-purple-100 rounded-full" :class="sizeConfig.track" />
@@ -162,7 +143,8 @@
 
       <!-- Valeur affichée au-dessus du curseur -->
       <div
-        class="absolute -translate-x-1/2 text-gray-700 font-medium"
+        v-if="isDragging || isHovering"
+        class="absolute -translate-x-1/2 text-gray-500"
         :class="[sizeConfig.text, sizeConfig.valueOffset]"
         :style="{ left: percentage + '%' }"
       >
@@ -171,7 +153,7 @@
 
       <!-- Curseur -->
       <div
-        class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-900 rounded-full"
+        class="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-500 rounded-sm"
         :class="sizeConfig.handle"
         :style="{ left: percentage + '%' }"
       />
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -3,21 +3,29 @@
   import InputRange from '~/components/common/input-range.vue';
 
   const isingStore = useIsingStore();
-  const { network_length } = storeToRefs(isingStore);
+  const { network_length, temperature, external_field } = storeToRefs(isingStore);
 </script>
 
 <template>
-  <div>
-    <div>
-      <InputRange
-        v-model="network_length"
-        :min="8"
-        :max="256"
-        :step="8"
-        place="left"
-        label="Taille réseau"
-        size="md"
-      />
-    </div>
+  <div class="flex flex-col gap-4">
+    <InputRange
+      v-model="network_length"
+      label="Taille de la grille"
+      :min="8"
+      :max="256"
+      :step="8"
+      size="sm"
+    />
+
+    <InputRange v-model="temperature" label="Température" :min="0" :max="300" :step="8" size="sm" />
+
+    <InputRange
+      v-model="external_field"
+      label="Champ magnétique externe"
+      :min="0"
+      :max="100"
+      :step="8"
+      size="sm"
+    />
   </div>
 </template>
