b713839c4073a484deedc3cf7a69d902d3570bb7
francoislang <langfrancoisfrederic@gmail.com>
2026-02-09 00:02:51 +0100

    feat(app): buffer damier

--- apps/web/components/ComponentIsingModel.vue
+++ apps/web/components/ComponentIsingModel.vue
@@ -4,17 +4,26 @@
 
 <template>
   <div class="flex flex-col h-screen w-full">
-    <div class="sticky top-0 flex border-b-2 border-foreground/10 bg-card p-4">
+    <div
+      class="sticky top-0 flex border-b-2 border-foreground/10 bg-card p-4 justify-between items-center text-foreground"
+    >
       <div class="flex flex-col">
-        <h1 class="text-foreground font-bold text-xl">Visualisation en temps réel</h1>
-        <span class="text-foreground text-xs italic">
-          Simulation Monte Carlo du modèle Ising 2D
-        </span>
+        <h1 class="font-bold text-xl">Visualisation en temps réel</h1>
+        <span class="text-xs italic">Simulation Monte Carlo du modèle Ising 2D</span>
       </div>
-      <div>
-        <span></span>
+      <div
+        class="flex items-center gap-2 border-2 border-green-300/10 bg-green-300 px-4 py-1 rounded-md"
+      >
+        <span class="w-2 h-2 bg-green-600 rounded-full"></span>
+        <span class="text-xs">En attente</span>
       </div>
     </div>
-    <ComponentLatticeView :pixelSize="4" />
+    <div class="p-4 justify-center items-center flex gap-4">
+      <CommonCard title="Grille de spins" subtitle="Bleu spin +1 | cyan spin -1" class="w-96">
+        <template #content>
+          <ComponentLatticeView :pixelSize="4" />
+        </template>
+      </CommonCard>
+    </div>
   </div>
 </template>
--- apps/web/components/ComponentLatticeView.vue
+++ apps/web/components/ComponentLatticeView.vue
@@ -8,6 +8,22 @@
   const isingStore = useIsingStore();
 
   const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+  onMounted(() => {
+    const canvas = canvasRef.value;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+
+    for (let i = 0; i < isingStore.network_length; i++) {
+      for (let j = 0; j < isingStore.network_length; j++) {
+        ctx.fillStyle = (i + j) % 2 === 0 ? '#0891b2' : '#f0fdfa';
+
+        ctx.fillRect(i * props.pixelSize, j * props.pixelSize, props.pixelSize, props.pixelSize);
+      }
+    }
+  });
 </script>
 
 <template>
