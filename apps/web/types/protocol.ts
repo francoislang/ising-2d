c40534e9c93e46a1ed9ce64c70399366d26ab15a
francoislang <langfrancoisfrederic@gmail.com>
2026-02-14 18:23:20 +0100

    wip(app): main to worker protocole works

--- apps/web/types/protocol.ts
+++ apps/web/types/protocol.ts
@@ -2,22 +2,74 @@ import type { InitModeType } from '~/types/isingStore.type';
 
 // main thread -> worker
 enum ProtocoleVersionConstant {
-  v1 = 'v1',
+  V1 = 'v1',
 }
 
 enum MessageConstant {
   INIT = 'init',
+  SETPARAMS = 'setParams',
+  START = 'start',
+  PAUSE = 'pause',
+  RESET = 'reset',
+  STEP = 'step',
+  SETSNAPSHOTRATE = 'setSnapshotRate',
+}
+
+interface IInitPayload {
+  network_length: number;
+  temperature: number;
+  external_field: number;
+  exchange_interaction: number;
+  seed: number;
+  initMode: InitModeType;
 }
 
 export interface IInitMessage {
   type: MessageConstant.INIT;
+  payload: IInitPayload;
+  version: ProtocoleVersionConstant.V1;
+}
+
+export interface ISetParamsMessage {
+  type: MessageConstant.SETPARAMS;
   payload: {
-    network_length: number;
     temperature: number;
     external_field: number;
-    exchange_interaction: number;
-    seed: number;
-    initMode: InitModeType;
   };
-  version: ProtocoleVersionConstant.v1;
 }
+
+export interface IStartMessage {
+  type: MessageConstant.START;
+}
+
+export interface IPauseMessage {
+  type: MessageConstant.PAUSE;
+}
+
+export interface IResetMessage {
+  type: MessageConstant.RESET;
+  payload: IInitPayload;
+}
+
+export interface IStepMessage {
+  type: MessageConstant.STEP;
+  payload: {
+    steps: number;
+  };
+}
+
+export interface ISetSnapshotRateMessage {
+  type: MessageConstant.SETSNAPSHOTRATE;
+  payload: {
+    rate: number;
+  };
+}
+
+export type MainToWorkerMessage =
+  | IInitMessage
+  | ISetParamsMessage
+  | IStartMessage
+  | IPauseMessage
+  | IResetMessage
+  | IStepMessage
+  | ISetSnapshotRateMessage;
